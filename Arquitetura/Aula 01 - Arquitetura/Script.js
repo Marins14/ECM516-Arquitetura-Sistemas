914e48c6a036d3a30b919c3d8591b59481b6200a
Marins14 <matheus.bernardello@live.com>
2023-02-25 17:32:14 -0300

    Colocando a pasta em seu devido repositório

---
+++ Arquitetura/Aula 01 - Arquitetura/Script.js
@@ -0,0 +1,60 @@
+//ECMAScript
+//const, let, var 
+// const nome1 = "Ana"
+// const nome2 = "Pedro"
+// const nome3 = "José"
+
+// console.log(typeof(nome1))
+// console.log(typeof(nome2))
+// console.log(typeof(nome3))
+//REPL : Read, Evaluate, Print, Loop
+
+// let a = 2 
+// let b = 'abc'
+// console.log(typeof(b))
+// b = 12 
+// console.log(typeof(b))
+// b = true
+// console.log(typeof(b))
+
+// var endereco = "Rua C"
+// console.log(endereco)
+// endereco = "Rua B"
+// console.log(endereco)
+// console.log(typeof(endereco))
+
+// var linguagem = "JavaScript"
+// console.log("Aprendendo"+ " " + linguagem)
+// linguagem = "Java"
+// console.log("Aprendendo"+ " " + linguagem)
+
+// var idade = 18
+// console.log("Oi, "+ nome)
+// if(idade>=18){
+//     var nome = "Joao"// hoist (joga a variavel p fora do if)
+//     console.log(nome + ", você pode dirigir")
+// }
+// console.log("Até mais, "+ nome)
+
+// const n1 = 2 
+// const n2 = '3'
+// //coerção implicita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explicita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+// console.log(1 == 1)
+// console.log(1 == '1')
+// console.log(1 === 1)
+// console.log(1 === '1')
+
+// console.log(true == 1)
+// console.log(1 == [1])
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == false)
+// console.log([] == [])
+// console.log(new Object()== new Object())
+// não se faz comparação em JS
