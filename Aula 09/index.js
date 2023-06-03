585d165aad1fb98486918aa0e7aa502daf88143f
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2023-06-03 09:24:47 -0300

    Add Pool

--- Aula 09/index.js
+++ Aula 09/index.js
@@ -7,25 +7,38 @@ const {
     PG_PORT,
     PORT
 } = process.env
-const { Client } = require('pg')
+const { Pool } = require('pg')
 const express = require('express')
 const { ssl } = require('pg/lib/defaults')
 const app = express()
 app.use(express.json())
 
-app.get('/medicos', (req,res) => {
-    const client = new Client({
+const pool = new Pool(
+    {
         host: PG_HOST,
         user: PG_USER,
         password: PG_PASSWORD,
         database: PG_DATABASE,
         port: PG_PORT
-    })
-    client.connect()
-    client.query('SELECT * FROM tb_medico')
+    } 
+    ) 
+    
+pool.on('error', (err, client) => {
+        console.error('Erro inesperado', err)
+        process.exit(-1)
+})
+
+app.get('/medicos', (req,res) => {
+    // const client = new Client({
+    //     host: PG_HOST,
+    //     user: PG_USER,
+    //     password: PG_PASSWORD,
+    //     database: PG_DATABASE,
+    //     port: PG_PORT
+    // })
+    pool.query('SELECT * FROM tb_medico')
     .then(db_Result => {
         res.status(200).json(db_Result.rows)
-        client.end()
     })
     .catch(e => {
         console.log(e)
@@ -34,55 +47,54 @@ app.get('/medicos', (req,res) => {
 })
 
 app.get('/paciente', (req,res) => {
-    const client = new Client({
-        host: PG_HOST,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        database: PG_DATABASE,
-        port: PG_PORT
-        })
-    client.connect()
-    client.query('SELECT * FROM tb_paciente')
-    .then(db_Result=>{
-        console.log(db_Result)
+    // const client = new Client({
+    //     host: PG_HOST,
+    //     user: PG_USER,
+    //     password: PG_PASSWORD,
+    //     database: PG_DATABASE,
+    //     port: PG_PORT
+    //     })
+    // client.connect()
+    // client.query('SELECT * FROM tb_paciente')
+    pool.query('SELECT * FROM tb_paciente')
+    .then(db_Result => {
         res.status(200).json(db_Result.rows)
     })
     .catch(e => {
         console.log(e)
         res.status(500).end()
-        client.end()
     })
 })
 
 app.post('/adicionar-medico', (req,res) => {
-    const client = new Client({
-        host: PG_HOST,
-        user: PG_USER,
-        password: PG_PASSWORD,
-        database: PG_DATABASE,
-        port: PG_PORT
-        })
+    // const client = new Client({
+    //     host: PG_HOST,
+    //     user: PG_USER,
+    //     password: PG_PASSWORD,
+    //     database: PG_DATABASE,
+    //     port: PG_PORT
+    //     })
     const crm = req.body.crm
     const nome = req.body.nome 
-    client.connect()
-    client.query(`INSERT INTO tb_medico (crm,nome) VALUES ($1,$2);`,[crm,nome])
+    //client.connect()
+    pool.query(`INSERT INTO tb_medico (crm,nome) VALUES ($1,$2);`,[crm,nome])
     .then(db_Result=>{
         console.log(db_Result)
-        res.status(200).json(db_Result.rows)
+        res.status(200).json("Funcionou")
     })
     .catch(e => {
         console.log(e)
         res.status(500).end()
-        client.end()
     })
 
 })
 
-
-
-
+    
 app.listen(
-    PORT,
-    () => console.log(`Servidor OK. Porta: ${PORT}`)
+        PORT,
+        () => console.log(`Servidor OK. Porta: ${PORT}`)
 )
+    
+
+
 
